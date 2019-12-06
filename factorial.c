39836bedf132a70a60cea4494618bdedde623841
amlan20 <48976718+amlan20@users.noreply.github.com>
2019-12-06 12:05:51 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#include<stdio.h>
 int main()
 {
-int i , n;
-unsigned long long fact = 1;
+int n,fact = 1;
 scanf("%d",&n);
 if (n == 1)
 {
@@ -13,11 +10,11 @@ return 1;
 }
 else
 {
-for(i=1;i<=n;i++)
+for(int i=1;i<=n;i++)
 {
 fact *= i;
 }
-printf( "factorial of %d is %llu",n,fact);
+printf( "factorial of %d is %d",n,fact);
 }
 return 0;
 }
